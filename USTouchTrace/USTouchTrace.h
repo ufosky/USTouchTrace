a576dda1027bdde8535da0f395d408369105ef82
Qihe Bian <ufosky@gmail.com>
2013-01-11 14:44:10 +0800

    Initial Commit

---
+++ USTouchTrace/USTouchTrace.h
@@ -0,0 +1,13 @@
+//
+//  USTouchTrace.h
+//  USTouchTrace
+//
+//  Created by Qihe Bian on 1/11/13.
+//  Copyright (c) 2013 Qihe Bian. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface USTouchTrace : NSObject
+
+@end
---
+++ USTouchTrace/USTouchTrace.m
@@ -0,0 +1,13 @@
+//
+//  USTouchTrace.m
+//  USTouchTrace
+//
+//  Created by Qihe Bian on 1/11/13.
+//  Copyright (c) 2013 Qihe Bian. All rights reserved.
+//
+
+#import "USTouchTrace.h"
+
+@implementation USTouchTrace
+
+@end
